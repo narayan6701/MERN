2aa6e62fed4f07e0538ea289cce6597a1fac9209
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-06 21:59:13 +0530

    require vs import

---
+++ 04_Node.js/Node.js - 1/math2.js
@@ -0,0 +1,6 @@
+// This file is made to work with require_vs_import.js
+
+export const sum =(a,b)=>a+b;
+export const mul =(a,b)=>a*b;
+export const g = 9.8;
+export const pi = 3.14;
---
+++ 04_Node.js/Node.js - 1/require_vs_import.js
@@ -0,0 +1,3 @@
+import { sum, pi } from "./math2.js";
+
+console.log(sum(1, 2));
