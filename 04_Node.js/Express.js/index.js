ca5ed9b696bc86b1097dd7bb36fefd1a5916fad0
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-10 19:53:16 +0530

    learned routing

--- 04_Node.js/Express.js/index.js
+++ 04_Node.js/Express.js/index.js
@@ -1,5 +1,5 @@
 // This file is going to act as a server.
-const express = require('express');
+const express = require("express");
 
 // "express" is a function that returns an object
 // it is a conventional practice to store the object in a variable called app
@@ -19,30 +19,55 @@ let port = 3000; //8080
 // Port 3000 is a common port used for development purposes. It is not reserved for any specific service or protocol making it a convenient choice for developers to use without conflicting with other services that might be running on the same server. they can be used to make custom servers.
 
 app.listen(port, () => {
-    console.log(`app listening on the port ${port}`);
+  console.log(`app listening on the port ${port}`);
 });
 
 // app.listen is a method that listens for incoming requests on a specific port. It has two parameters, the first one is the port number and the second one is a callback function that will be called when the app starts listening on the given port.
 
-app.use((req, res)=>{
-    console.log("we got a request");
-    console.log(req);
-    console.log("This is a response");
-    console.log(res);
-    // res.send("This is a response");
-    // res.send({
-    //     name:"apple",
-    //     color:"red"
-    // })
-    let code = "<h1>Fruits</h1> <ul> <li>Apple</li> <li>Orange</li></ul>"
-    res.send(code);
-});
+// app.use((req, res) => {
+//   console.log("we got a request");
+//   console.log(req);
+//   console.log("This is a response");
+//   console.log(res);
+//   // res.send("This is a response");
+//   // res.send({
+//   //     name:"apple",
+//   //     color:"red"
+//   // })
+//   let code = "<h1>Fruits</h1> <ul> <li>Apple</li> <li>Orange</li></ul>";
+//   res.send(code);
+// });
 
 // app.use is a method in Express.js used to set up middleware functions. Middleware functions are functions that have access to the request object (req), the response object (res), and the next function in the application's request-response cycle. These functions can perform various tasks, such as modifying the request or response objects, ending the request-response cycle, or calling the next middleware function in the stack.
 
-// Whenever we send a request to the server, the http request is text based request, such that servers made using different technologies are able to understand the request. 
+// Whenever we send a request to the server, the http request is text based request, such that servers made using different technologies are able to understand the request.
 
 // But Express.js converts that text based request into a JavaScript object called req and it also converts the response into a JavaScript object called res. This process is called parsing.
 
 // res.send() is a method in Express.js used to send a response in the JSON format to the client. It can be used to send a string, an object, an array, html code or a buffer as a response. When res.send() is called, Express.js automatically sets the Content-Type header based on the type of the response.
 
+//app.get is a method in Express.js used to handle GET requests. It takes two parameters, the first one is the path of the request and the second one is a callback function that will be called when the app receives a GET request on the specified path.
+
+app.get("/", (req, res) => {
+  res.send("Contacted root path");
+});
+
+app.get("/apple", (req, res) => {
+  res.send("Contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+  res.send("Contacted orange path");
+});
+
+// setting path to "*" means if the request is for an invalid route, then this route will be called.
+
+app.get("*", (req, res)=>{
+  res.send("Page not found");
+})
+
+// app.post is used to handle POST requests. It takes two parameters, the first one is the path of the request and the second one is a callback function that will be called when the app receives a POST request on the specified path.
+
+app.post("/", (req, res) => {
+  res.send("you sent a post request on root path");
+});
