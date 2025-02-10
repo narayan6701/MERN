ed0309a5c3d495168cea00fc68ab88c0cb485211
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-10 18:59:44 +0530

    Learned about res.send

--- 04_Node.js/Express.js/index.js
+++ 04_Node.js/Express.js/index.js
@@ -26,6 +26,23 @@ app.listen(port, () => {
 
 app.use((req, res)=>{
     console.log("we got a request");
+    console.log(req);
+    console.log("This is a response");
+    console.log(res);
+    // res.send("This is a response");
+    // res.send({
+    //     name:"apple",
+    //     color:"red"
+    // })
+    let code = "<h1>Fruits</h1> <ul> <li>Apple</li> <li>Orange</li></ul>"
+    res.send(code);
 });
 
 // app.use is a method in Express.js used to set up middleware functions. Middleware functions are functions that have access to the request object (req), the response object (res), and the next function in the application's request-response cycle. These functions can perform various tasks, such as modifying the request or response objects, ending the request-response cycle, or calling the next middleware function in the stack.
+
+// Whenever we send a request to the server, the http request is text based request, such that servers made using different technologies are able to understand the request. 
+
+// But Express.js converts that text based request into a JavaScript object called req and it also converts the response into a JavaScript object called res. This process is called parsing.
+
+// res.send() is a method in Express.js used to send a response in the JSON format to the client. It can be used to send a string, an object, an array, html code or a buffer as a response. When res.send() is called, Express.js automatically sets the Content-Type header based on the type of the response.
+
