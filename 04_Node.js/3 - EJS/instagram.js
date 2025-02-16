d4ae11a41c1a06b687c43079627d6749c77e304d
Narayan Bhardwaj <narayan6701@outlook.in>
2025-02-17 00:13:08 +0530

    Serving Static Files

--- 04_Node.js/3 - EJS/instagram.js
+++ 04_Node.js/3 - EJS/instagram.js
@@ -1,37 +1,35 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  app.use(express.static(path.join(__dirname, "/public/JS")));
+  app.use(express.static(path.join(__dirname, "/public/CSS")));
+  // It gives EJS files direct access to the public folder
   app.set("view engine", "ejs");
 });
+
+// we should have a folder named called public in the root directory of the project. This folder will contain all the static files that we want to serve.
 
 app.get("/", (req, res) => {
   res.send("This is home");
 });
 
 app.get("/ig/:username", (req, res) => {
-
-    let {username} = req.params;
+  let { username } = req.params;
 
   const insta_data = require("./data.json");
   const data = insta_data[username];
   console.log(data);
-  if(data){
-    res.render("insta_with_data.ejs", {data});
-  }
-  else{
+  if (data) {
+    res.render("insta_with_data.ejs", { data });
+  } else {
     res.render("error.ejs");
   }
-  
-  
 });
 
-
-
-
-
-app.use((req, res) => {
-    res.send("Invalid Route");
-  });
+// app.use((req, res) => {
+//     res.send("Invalid Route");
+//   });
