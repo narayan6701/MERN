2e71cf76b99036179e512fb1a3359ec7f243c9b6
Narayan Bhardwaj <narayan6701@outlook.in>
2025-04-15 23:21:17 +0530

    learning mongoose

--- 04_JS Frameworks/6 - Mongo DB/index.js
+++ 04_JS Frameworks/6 - Mongo DB/index.js
@@ -1,13 +1,58 @@
 const mongoose = require("mongoose");
 
 main()
-.then(() => {
-	console.log("connection successfull");
-	})
-.catch((err) => {
-	console.log(err)
-	});
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-async function main(){
-	await mongoose.connect("mongodb://127.0.0.1:27017/test");
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
 }
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  emaill: String,
+  age: Number,
+});
+
+const User = mongoose.model("User", userSchema);
+// const Employee = mongoose.model("Employee", userSchema);
+
+// const user1 = new User({
+// 	name:"Narayan",
+// 	email:"narayan6701@outlook.in",
+// 	age:48
+// });
+
+// const user2 = new User({
+// 	name:"hari",
+// 	email:"hari6701@outlook.in",
+// 	age:48
+// });
+
+// user1.save();
+
+// user2.save()
+// .then((res) => {
+// 	console.log(res);
+// })
+// .catch(err=>{
+// 	console.log(err)
+// });
+
+// User.insertMany([
+//   {name:"Tony", emal:"tony@gmail.com", age:50},
+//   {name: "Peter", email:"peter@gmail.com", age:30},
+//   {name:"Bruce", email:"bruce@gmail.com", age:40}
+// ]).then((res) => {
+//   console.log(res);
+// });
+
+User.find({age:{$gt:47}}).then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.log(err);
+});
