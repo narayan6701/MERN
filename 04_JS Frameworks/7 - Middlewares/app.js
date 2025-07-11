99b4bc153e388b14dbdc8e16d51b3a1a162ea2e2
Narayan Bhardwaj <narayan6701@outlook.in>
2025-07-11 15:27:31 +0530

    created utility middleware

--- 04_JS Frameworks/7 - Middlewares/app.js
+++ 04_JS Frameworks/7 - Middlewares/app.js
@@ -3,17 +3,23 @@ const app = express();
 
 // Middleware sends response immediately
 
-app.use((req,res, next)=> {
-    console.log("Middleware 1 executed");
-    next();
-    // console.log("this is after next in Middleware 1");
-    // return next();
-});
+// app.use((req,res, next)=> {
+//     console.log("Middleware 1 executed");
+//     next();
+//     // console.log("this is after next in Middleware 1");
+//     // return next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("Middleware 2 executed");
+//     next();
+// }); 
 
 app.use((req, res, next) => {
-    console.log("Middleware 2 executed");
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
     next();
-}); 
+});
 
 app.get("/", (req, res) => {
     res.send("hi i am root");
@@ -25,3 +31,4 @@ app.get("/random", (req,res) => {
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
 });
+
